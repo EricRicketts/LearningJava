62ab9c8bc070182b102d549257bfc38dc71d4c04
EricRicketts <eric_ricketts@me.com>
2021-08-23 09:06:46 -0400

    three dimensional array

--- JavaABeginnersGuideEighthEdition/Chapter5/TwoDimensionalArray/src/test/java/TwoDimensionalArrayTest.java
+++ JavaABeginnersGuideEighthEdition/Chapter5/TwoDimensionalArray/src/test/java/TwoDimensionalArrayTest.java
@@ -43,4 +43,15 @@ class TwoDimensionalArrayTest {
         assertEquals(1, example[0][0]);
         assertEquals(6, example[2][3]);
     }
+
+    @Test
+    void threeDimensionalArray() {
+        int example[][][] = {{{2, 4, 6}, {8, 10, 12}, {14, 16, 18}}, {{20, 22, 24}, {26, 28, 30}, {32, 34, 36}},
+                {{38, 40, 42}, {44, 46, 48}, {50, 52, 54}}};
+        int lastElement = example[2][2][2];
+        int[] lastXY= example[2][2];
+        assertEquals(54, lastElement);
+        int expected[] = {50, 52, 54};
+        assertArrayEquals(expected, lastXY);
+    }
 }
