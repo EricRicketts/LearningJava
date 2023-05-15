9eb3778147e8d8c7f477e0eb9d5775f1ad5ac605
Eric Ricketts <eric_ricketts@me.com>
2023-05-15 04:17:28 -0400

    add testing.xml file and add comments to test file

--- TestNGVideos/LearnTestNG/src/test/java/org/example/PersonTest.java
+++ TestNGVideos/LearnTestNG/src/test/java/org/example/PersonTest.java
@@ -2,7 +2,7 @@ package org.example;
 
 import org.testng.Assert;
 import org.testng.annotations.*;
-import org.testng.Assert.*;
+import static org.testng.Assert.*;
 
 public class PersonTest {
     Person firstPerson;
@@ -11,13 +11,31 @@ public class PersonTest {
         firstPerson = new Person("Elmer", "Fudd", 35);
     }
 
-    @Test
+    @Test(priority = 1)
     public void testGetAge() {
-        Assert.assertEquals(35, firstPerson.getAge());
+        assertEquals(35, firstPerson.getAge());
     }
 
-    @Test
+    @Test(priority = 4)
     public void testGetFullName() {
         Assert.assertEquals("Elmer Fudd", firstPerson.getFullName());
     }
+
+    @Test(priority = 2)
+    public void testGetFirstName() {
+        assertEquals("Elmer", firstPerson.getFirstName());
+    }
+
+    @Test(priority = 3)
+    public void testGetLastName() {
+        assertEquals("Fudd", firstPerson.getLastName());
+    }
 }
+
+/*
+    Normally, if no parameters are given to the @Test annotation then TestNG will run
+    the tests in alphabetical order, with the priority parameters set then the tester
+    can control in what order the tests are run.  In this case the order is: testGetAge,
+    testGetFirstName, testGetLastName, and testGetFullName.  One will see this order
+    being run in the IDE.
+*/
