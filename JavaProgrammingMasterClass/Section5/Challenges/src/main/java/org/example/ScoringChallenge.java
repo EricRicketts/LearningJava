e5cda0cc16cf704e594be36227fa7b3d11723958
Eric Ricketts <eric_ricketts@me.com>
2023-07-08 07:50:40 -0400

    first test for the scoring challenge score is 1500

---
+++ JavaProgrammingMasterClass/Section5/Challenges/src/main/java/org/example/ScoringChallenge.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class ScoringChallenge {
+
+    public static int calculateHighScorePosition(int score) {
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500 ) {
+            return 2; 
+        } else if (score >= 100) {
+            return 3;
+        }
+        return 4;
+    }
+    public static String displayHighScorePosition(String name, int position) {
+        return name + " managed to get into position " + position + " on the high score list";
+    }
+}
--- JavaProgrammingMasterClass/Section5/Challenges/src/test/java/org/example/ChallengesSection5Test.java
+++ JavaProgrammingMasterClass/Section5/Challenges/src/test/java/org/example/ChallengesSection5Test.java
@@ -16,6 +16,8 @@ public class ChallengesSection5Test {
     private Integer bonus;
     private Integer expectedFinalScore;
     private Integer finalScore;
+    private String expectedNameAndPosition;
+    private String resultantNameAndPosition;
 
     @BeforeEach
     public void setUp() {
@@ -45,5 +47,18 @@ public class ChallengesSection5Test {
             setFinalScore(getScore() + getLevelCompleted() * getBonus());
         }
         Assertions.assertEquals(expectedFinalScore, finalScore);
+    }
+
+    @Test
+    public void testForScoreOf1500() {
+        setExpectedNameAndPosition("Eric managed to get into position 1 on the high score list");
+        setResultantNameAndPosition(
+                ScoringChallenge.displayHighScorePosition(
+                        "Eric",
+                        ScoringChallenge.calculateHighScorePosition(1500)
+                )
+        );
+
+        Assertions.assertEquals(getExpectedNameAndPosition(), getResultantNameAndPosition());
     }
 }
