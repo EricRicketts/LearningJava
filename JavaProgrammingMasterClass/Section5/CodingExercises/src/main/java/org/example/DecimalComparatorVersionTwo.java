3d902cfded2640656ebe9518b4fba2c8e8a6b6bf
Eric Ricketts <eric_ricketts@me.com>
2023-07-10 18:01:56 -0400

    refactor second version of Decimal Comparator but really it is just renaming local variables and spacing the code out a bit more

--- JavaProgrammingMasterClass/Section5/CodingExercises/src/main/java/org/example/DecimalComparatorVersionTwo.java
+++ JavaProgrammingMasterClass/Section5/CodingExercises/src/main/java/org/example/DecimalComparatorVersionTwo.java
@@ -1,22 +1,38 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DecimalComparatorVersionTwo {
+
     public static boolean areEqualByThreeDecimalPlaces(double firstNumber, double secondNumber) {
-        String stringOne = String.valueOf(firstNumber);
-        String stringTwo = String.valueOf(secondNumber);
-        int stringOneLength = stringOne.length();
-        int stringTwoLength = stringTwo.length();
-        int decimalIndexStringOne = stringOne.indexOf(".");
-        int decimalIndexStringTwo = stringTwo.indexOf(".");
-        String decimalsStringOne = stringOne.substring(decimalIndexStringOne + 1, stringOneLength);
-        String decimalsStringTwo = stringTwo.substring(decimalIndexStringTwo + 1, stringTwoLength);
+        List<String> numberOfDecimalsForEachNumber = new ArrayList<>();
+        String firstNumberString = String.valueOf(firstNumber), secondNumberString = String.valueOf(secondNumber);
+        int decimalIndexFirstNumberString = firstNumberString.indexOf(".");
+        int decimalIndexSecondNumberString= secondNumberString.indexOf(".");
 
-        int shortestDecimalLength = Math.min(Math.min(decimalsStringOne.length(), decimalsStringTwo.length()), 3);
-        stringOne = stringOne.substring(0, decimalIndexStringOne) +
-                stringOne.substring(decimalIndexStringOne, decimalIndexStringOne + shortestDecimalLength + 1);
-        stringTwo = stringTwo.substring(0, decimalIndexStringTwo) +
-                stringTwo.substring(decimalIndexStringTwo, decimalIndexStringTwo + shortestDecimalLength + 1);
+        // find the number of decimals for the string representation of each number
+        String decimalsFirstNumberString =
+                firstNumberString.substring(decimalIndexFirstNumberString + 1);
+        String decimalsSecondNumberString =
+                secondNumberString.substring(decimalIndexSecondNumberString + 1);
 
-        return stringOne.equals(stringTwo);
+        // find the least of amount of decimals compare the first number, second number, and 3
+        int shortestDecimalLength = Math.min(
+                Math.min(decimalsFirstNumberString.length(), decimalsSecondNumberString.length()),
+                3);
+
+        // construct the final string representation for each number concatenate the beginning of each
+        // number to the decimal point and then from the decimal point to the end of the number
+        firstNumberString = firstNumberString.substring(0, decimalIndexFirstNumberString) +
+                firstNumberString.substring(decimalIndexFirstNumberString,
+                        decimalIndexFirstNumberString + shortestDecimalLength + 1);
+
+        secondNumberString = secondNumberString.substring(0, decimalIndexSecondNumberString) +
+                secondNumberString.substring(decimalIndexSecondNumberString,
+                        decimalIndexSecondNumberString + shortestDecimalLength + 1);
+
+        return firstNumberString.equals(secondNumberString);
     }
 }
