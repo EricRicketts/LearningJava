10343b803a9236f9f4d30b1e7360598b8270c049
Eric Ricketts <eric_ricketts@me.com>
2023-11-18 04:45:36 -0500

    add course test cases to postive, negative, or zero code

--- JavaProgrammingMasterClass/Section5/CodingExercises/src/main/java/org/example/PositiveNegativeZero.java
+++ JavaProgrammingMasterClass/Section5/CodingExercises/src/main/java/org/example/PositiveNegativeZero.java
@@ -2,11 +2,13 @@ package org.example;
 
 public class PositiveNegativeZero {
     public static String checkNumber(int number) {
-        String sign = "zero";
+        String sign = "";
         if (number > 0) {
             sign = "positive";
         } else if (number < 0) {
             sign = "negative";
+        } else {
+            sign = "zero";
         }
         return sign;
     }
--- JavaProgrammingMasterClass/Section5/CodingExercises/src/test/java/org/example/PositiveNegativeZeroTest.java
+++ JavaProgrammingMasterClass/Section5/CodingExercises/src/test/java/org/example/PositiveNegativeZeroTest.java
@@ -19,4 +19,16 @@ public class PositiveNegativeZeroTest {
     public void testForNegative() {
         Assertions.assertEquals("negative", PositiveNegativeZero.checkNumber(-1));
     }
+
+    @Test
+    public void testCasesFromCourse() {
+        int[] values = new int[]{1, 3278, -1, -14, 0};
+        String[] answers =
+                new String[]{"positive", "positive", "negative", "negative", "zero"};
+        for (int index = 0; index < values.length; index++) {
+            int value = values[index];
+            String answer = answers[index];
+            Assertions.assertEquals(answer, PositiveNegativeZero.checkNumber(value));
+        }
+    }
 }
