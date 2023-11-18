1e7f2f724a64d44eecff163f229b251dda68a122
Eric Ricketts <eric_ricketts@me.com>
2023-11-18 06:25:34 -0500

    add course test cases for barking dog

--- JavaProgrammingMasterClass/Section5/CodingExercises/src/test/java/org/example/BarkingDogTest.java
+++ JavaProgrammingMasterClass/Section5/CodingExercises/src/test/java/org/example/BarkingDogTest.java
@@ -24,4 +24,25 @@ public class BarkingDogTest {
     public void testInvalidTimeInput() {
         Assertions.assertFalse(BarkingDog.shouldWakeUp(true, -1));
     }
+
+    @Test
+    public void testCasesFromCourse() {
+        boolean[] firstArgumentValues = new boolean[]{
+                false, true, false, true, false, true,
+                true, true, true, true, true
+        };
+        int[] secondArgumentValues = new int[]{-5, -1, 24, 44, 6, 4, 23, 22, 8, 7, 0};
+        boolean[] answers = new boolean[]{
+                false, false, false, false, false, true,
+                true, false, false, true, true
+        };
+        for (int index = 0; index < answers.length; index++) {
+            boolean firstArgumentValue = firstArgumentValues[index];
+            int secondArgumentValue = secondArgumentValues[index];
+            boolean answer = answers[index];
+            Assertions.assertEquals(
+                answer, BarkingDog.shouldWakeUp(firstArgumentValue, secondArgumentValue)
+            );
+        }
+    }
 }
