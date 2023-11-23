2109c760d42edb827262fd8a7e9c702b1183e4ae
Eric Ricketts <eric_ricketts@me.com>
2023-11-23 11:22:06 -0500

    teen number checker all course test cases for isTeen

--- JavaProgrammingMasterClass/Section5/CodingExercises/src/test/java/org/example/TeenNumberCheckerTest.java
+++ JavaProgrammingMasterClass/Section5/CodingExercises/src/test/java/org/example/TeenNumberCheckerTest.java
@@ -38,19 +38,69 @@ public class TeenNumberCheckerTest {
     }
 
     @Test
-    public void testCasesFromCourse() {
+    public void testCasesFromCourseForIsTeen() {
+        List<Integer> ages = new ArrayList<>(Arrays.asList(
+                7, 11, 12, 13,
+                14, 15, 16, 17,
+                18, 19, 20, 21,
+                22, 36, 122, 54,
+                0, -1, -2, -3
+            )
+        );
+        List<Boolean> results = new ArrayList<>(Arrays.asList(
+                false, false, false, true,
+                true, true, true, true,
+                true, true, false, false,
+                false, false, false, false,
+                false, false, false, false
+            )
+        );
+        for (int index = 0; index < ages.size(); index++) {
+            Integer age = ages.get(index);
+            boolean result = results.get(index);
+            Assertions.assertEquals(
+                result,
+                TeenNumberChecker.isTeen(age)
+            );
+        }
+    }
+
+    @Test
+    public void testCasesFromCourseForHasTeen() {
         List<List<Integer>> allAges = new ArrayList<>(Arrays.asList(
                 Arrays.asList(0, 0, 0), Arrays.asList(0, 0, -1), Arrays.asList(0, -1, 0),
                 Arrays.asList(0, -1, -1), Arrays.asList(-1, 0, 0), Arrays.asList(-1, 0, -1),
-                Arrays.asList(-1, -1, 0), Arrays.asList(-1, -1, -1), Arrays.asList(0, 0, 0)
-
+                Arrays.asList(-1, -1, 0), Arrays.asList(-1, -1, -1), Arrays.asList(0, 0, 0),
+                Arrays.asList(0, 0, -5), Arrays.asList(0, -5, 0), Arrays.asList(0, -5, -5),
+                Arrays.asList(-5, 0, 0), Arrays.asList(-5, 0, -5), Arrays.asList(-5, -5, 0),
+                Arrays.asList(-5, -5, -5), Arrays.asList(0, 0, 0), Arrays.asList(0, 0, 0),
+                Arrays.asList(0, 0, 0), Arrays.asList(0, 0, 0), Arrays.asList(0, 0, 0),
+                Arrays.asList(0, 0, 0), Arrays.asList(0, 0, 0), Arrays.asList(0, 0, 0),
+                Arrays.asList(0, 0, 0), Arrays.asList(0, 0, 12), Arrays.asList(0, 12, 0)
             )
         );
         List<Boolean> results = new ArrayList<>(Arrays.asList(
                 false, false, false,
                 false, false, false,
+                false, false, false,
+                false, false, false,
+                false, false, false,
+                false, false, false,
+                false, false, false,
+                false, false, false,
                 false, false, false
             )
         );
+        for (int index = 0; index < allAges.size(); index++) {
+            Integer firstAgeIndex = 0, secondAgeIndex = 1, thirdAgeIndex = 2;
+            List<Integer> ages = allAges.get(index);
+            Integer firstAge = ages.get(firstAgeIndex);
+            Integer secondAge = ages.get(secondAgeIndex);
+            Integer thirdAge = ages.get(thirdAgeIndex);
+            boolean result = results.get(index);
+            Assertions.assertEquals(
+                result, TeenNumberChecker.hasTeen(firstAge, secondAge, thirdAge)
+            );
+        }
     }
 }
