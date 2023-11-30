7fa01c31df98fc76d54b44145913b5a9de80b5c2
Eric Ricketts <eric_ricketts@me.com>
2023-11-30 18:36:31 -0500

    complete coding exercise for number of days in month

--- JavaProgrammingMasterClass/Section6/CodingExercises/src/test/java/org/example/NumberOfDaysInMonthTest.java
+++ JavaProgrammingMasterClass/Section6/CodingExercises/src/test/java/org/example/NumberOfDaysInMonthTest.java
@@ -63,4 +63,74 @@ public class NumberOfDaysInMonthTest {
             );
         }
     }
+
+    @Test
+    public void testCourseLeapYearData() {
+        List<Integer> years = new ArrayList<>(Arrays.asList(
+                -1024, 0, 10_000, 9_000,
+                1800, 1855, 1846, 1870,
+                1900, 1924, 1944, 1980,
+                2000, 2014, 2016, 2020,
+                2100, 2104, 2140, 2150
+            )
+        );
+        List<Boolean> results = new ArrayList<>(Arrays.asList(
+                false, false, false, false,
+                false, false, false, false,
+                false, true, true, true,
+                true, false, true, true,
+                false, true, true, false
+            )
+        );
+
+        for (int index = 0; index < results.size(); index++) {
+            int year = years.get(index);
+            boolean result = results.get(index);
+            Assertions.assertEquals(
+                    result,
+                    NumberOfDaysInMonth.isLeapYear(year)
+            );
+        }
+    }
+
+    @Test
+    public void testCourseDaysInMonthData() {
+        List<Integer> months = new ArrayList<>(Arrays.asList(
+                1, 2, 2, 2,
+                2, 2, 3, 4,
+                5, 6, 7, 8,
+                9, 10, 11, 12,
+                12, -1, 0, 22,
+                1, 13, -1
+            )
+        );
+        List<Integer> years = new ArrayList<>(Arrays.asList(
+                2020, 2020, 2018, 2016,
+                2000, 1900, 2020, 2020,
+                2020, 2020, 2020, 2020,
+                2020, 2020, 2020, 2020,
+                10_000, 2020, 2020, 2020,
+                -2020, -2020, -2020
+            )
+        );
+        List<Integer> daysInMonthList = new ArrayList<>(Arrays.asList(
+                31, 29, 28, 29,
+                29, 28, 31, 30,
+                31, 30, 31, 31,
+                30, 31, 30, 31,
+                -1, -1, -1, -1,
+                -1, -1, -1
+            )
+        );
+
+        for (int index = 0; index < daysInMonthList.size(); index++) {
+            int month = months.get(index);
+            int year = years.get(index);
+            int daysInMonth = daysInMonthList.get(index);
+            Assertions.assertEquals(
+                    daysInMonth,
+                    NumberOfDaysInMonth.getDaysInMonth(month, year)
+            );
+        }
+    }
 }
