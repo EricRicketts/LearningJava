5c5e31539e7aa9994bacbdd7e61d0d6d880682b5
Eric Ricketts <eric_ricketts@me.com>
2023-12-03 09:25:55 -0500

    Last Digit Checker Coding Exercise

--- JavaProgrammingMasterClass/Section6/CodingExercises/src/main/java/org/example/LastDigitChecker.java
+++ JavaProgrammingMasterClass/Section6/CodingExercises/src/main/java/org/example/LastDigitChecker.java
@@ -5,4 +5,18 @@ public class LastDigitChecker {
     public static boolean isValid(int number) {
         return (number >= 10 && number <= 1000);
     }
+
+    public static boolean hasSameLastDigit(int firstNumber, int secondNumber, int thirdNumber) {
+        boolean hasSameLastDigit = false;
+        if (!isValid(firstNumber) || !isValid(secondNumber) || !isValid(thirdNumber)) return false;
+        int firstNumberLastDigit = firstNumber % 10;
+        int secondNumberLastDigit = secondNumber % 10;
+        int thirdNumberLastDigit = thirdNumber % 10;
+
+        if ((firstNumberLastDigit == secondNumberLastDigit) ||
+                (firstNumberLastDigit == thirdNumberLastDigit) ||
+                (secondNumberLastDigit == thirdNumberLastDigit)) hasSameLastDigit = true;
+
+        return hasSameLastDigit;
+    }
 }
--- JavaProgrammingMasterClass/Section6/CodingExercises/src/test/java/org/example/LastDigitCheckerTest.java
+++ JavaProgrammingMasterClass/Section6/CodingExercises/src/test/java/org/example/LastDigitCheckerTest.java
@@ -29,4 +29,91 @@ public class LastDigitCheckerTest {
             );
         }
     }
+
+    @Test
+    public void testHasSameLastDigit() {
+        List<Integer> firstNumbers = new ArrayList<>(Arrays.asList(
+                41, 23, 65, 9, 1001
+        ));
+        List<Integer> secondNumbers = new ArrayList<>(Arrays.asList(
+                22, 32, 45, 99, 700
+        ));
+        List<Integer> thirdNumbers = new ArrayList<>(Arrays.asList(
+                71, 42, 77, 999, 800
+        ));
+        List<Boolean> results = new ArrayList<>(Arrays.asList(
+                true, true, true, false, false
+        ));
+
+        for (int index = 0; index < results.size(); index++) {
+            int firstNumber = firstNumbers.get(index);
+            int secondNumber = secondNumbers.get(index);
+            int thirdNumber = thirdNumbers.get(index);
+            boolean result = results.get(index);
+
+            Assertions.assertEquals(
+                    result,
+                    LastDigitChecker.hasSameLastDigit(firstNumber, secondNumber, thirdNumber)
+            );
+        }
+    }
+
+    @Test
+    public void testCourseDataForIsValid() {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(
+                9, 10, 11, -1, 23,
+                41, 224, -411, 1081, 777,
+                1001, 1000, 6272
+        ));
+        List<Boolean> results = new ArrayList<>(Arrays.asList(
+                false, true, true, false, true,
+                true, true, false, false, true,
+                false, true, false
+        ));
+
+        for (int index = 0; index < results.size(); index++) {
+            int number = numbers.get(index);
+            boolean result = results.get(index);
+            Assertions.assertEquals(
+                    result,
+                    LastDigitChecker.isValid(number)
+            );
+        }
+    }
+
+    @Test
+    public void testCourseDataForHasSameLastDigit() {
+        List<Integer> firstNumbers = new ArrayList<>(Arrays.asList(
+                9, 1, 15, 15, 10,
+                11, -1, 23, 41, 22,
+                -41, 10, 777, 1001, 62
+        ));
+        List<Integer> secondNumbers = new ArrayList<>(Arrays.asList(
+                99, 1, 9, 10, 10,
+                22, 11, 32, 22, 23,
+                50, 11, 771, 771, 882
+        ));
+        List<Integer> thirdNumbers = new ArrayList<>(Arrays.asList(
+                19, 2, 20, 9, 10,
+                31, 11, 42, 71, 34,
+                31, 81, 77, 77, 772
+        ));
+        List<Boolean> results = new ArrayList<>(Arrays.asList(
+                false, false, false, false, true,
+                true, false, true, true, false,
+                false, true, true, false, true
+        ));
+
+        for (int index = 0; index < results.size(); index++) {
+            int firstNumber = firstNumbers.get(index);
+            int secondNumber = secondNumbers.get(index);
+            int thirdNumber = thirdNumbers.get(index);
+            boolean result = results.get(index);
+
+            Assertions.assertEquals(
+                    result,
+                    LastDigitChecker.hasSameLastDigit(firstNumber, secondNumber, thirdNumber)
+            );
+        }
+   }
 }
