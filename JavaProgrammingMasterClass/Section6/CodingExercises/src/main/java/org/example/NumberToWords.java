51cf8a48221659f53fc376318bcbb1415d8a9405
Eric Ricketts <eric_ricketts@me.com>
2023-12-06 06:08:47 -0500

    Number to words gitDigitCount and reverse

--- JavaProgrammingMasterClass/Section6/CodingExercises/src/main/java/org/example/NumberToWords.java
+++ JavaProgrammingMasterClass/Section6/CodingExercises/src/main/java/org/example/NumberToWords.java
@@ -4,6 +4,91 @@ public class NumberToWords {
 
     public static int getDigitCount(int number) {
         if (number < 0) return -1;
-        return Integer.toString(number).length();
+        if (number == 0) return 1;
+        int digitCount = 0;
+        while (number > 0) {
+            digitCount = digitCount + 1;
+            number = number / 10;
+        }
+        return digitCount;
+    }
+
+    public static int reverse(int number) {
+        int reversedNumber = 0;
+        int numberAbsoluteValue = Math.abs(number);
+        int numberOfDigits = getDigitCount(numberAbsoluteValue);
+        for (int index = 0; index < numberOfDigits; index++) {
+            int digit = numberAbsoluteValue % 10;
+            if (index == numberOfDigits - 1) {
+                reversedNumber = reversedNumber + digit;
+            } else {
+                reversedNumber = (reversedNumber + digit) * 10;
+            }
+            numberAbsoluteValue = numberAbsoluteValue / 10;
+        }
+        if (number < 0) reversedNumber = -1 * reversedNumber;
+        return reversedNumber;
+    }
+
+    public static String getNumberToWords(int number) {
+        int digitCount = getDigitCount(number);
+        if (digitCount < 0) return "Invalid Value";
+        int reversedNumber = reverse(number);
+        String numberInWords = "";
+        for (int index = 0; index < digitCount; index++) {
+            int digit = reversedNumber % 10;
+            numberInWords = numberInWords + switch(digit) {
+                case 0 -> "Zero ";
+                case 1 -> "One ";
+                case 2 -> "Two ";
+                case 3 -> "Three ";
+                case 4 -> "Four ";
+                case 5 -> "Five ";
+                case 6 -> "Six ";
+                case 7 -> "Seven ";
+                case 8 -> "Eight ";
+                case 9 -> "Nine ";
+                default -> "Invalid Value";
+            };
+            reversedNumber = reversedNumber / 10;
+        }
+        numberInWords = numberInWords.substring(0, numberInWords.length() - 1);
+        return numberInWords;
+    }
+
+    public static void numberToWords(int number) {
+        int digitCount = getDigitCount(number);
+        if (digitCount < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+        int reversedNumber = reverse(number);
+        String numberInWords = "";
+        for (int index = 0; index < digitCount; index++) {
+            int digit = reversedNumber % 10;
+            numberInWords = numberInWords + switch(digit) {
+                case 0 -> "Zero ";
+                case 1 -> "One ";
+                case 2 -> "Two ";
+                case 3 -> "Three ";
+                case 4 -> "Four ";
+                case 5 -> "Five ";
+                case 6 -> "Six ";
+                case 7 -> "Seven ";
+                case 8 -> "Eight ";
+                case 9 -> "Nine ";
+                default -> "Invalid Value";
+            };
+            reversedNumber = reversedNumber / 10;
+        }
+        numberInWords = numberInWords.substring(0, numberInWords.length() - 1);
+        System.out.println(numberInWords);
     }
 }
+/*
+    1234 => 4321
+    (0 + 4) * 10 = 40
+    (40 + 3) * 10 = 430
+    (430 + 2) * 10 = 4320
+    (4320 + 1) = 4321
+*/
--- JavaProgrammingMasterClass/Section6/CodingExercises/src/test/java/org/example/NumberToWordsTest.java
+++ JavaProgrammingMasterClass/Section6/CodingExercises/src/test/java/org/example/NumberToWordsTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public class NumberToWordsTest {
 
+    String expected;
     @Test
     public void testGetDigitCount() {
         List<Integer> numbers = new ArrayList<>(Arrays.asList(
@@ -23,6 +24,111 @@ public class NumberToWordsTest {
             Assertions.assertEquals(
                     numberOfDigits,
                     NumberToWords.getDigitCount(number)
+            );
+        }
+    }
+
+    @Test
+    public void testReverse() {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(
+                -121, 1212, 1234, 100
+        ));
+        List<Integer> reversedNumbers = new ArrayList<>(Arrays.asList(
+                -121, 2121, 4321, 1
+        ));
+
+        for (int index = 0; index < numbers.size(); index++) {
+            int number = numbers.get(index);
+            int reversedNumber = reversedNumbers.get(index);
+            Assertions.assertEquals(
+                    reversedNumber,
+                    NumberToWords.reverse(number)
+            );
+        }
+    }
+
+    @Test
+    public void testOneTwoThreeFour() {
+        expected = "One Two Three Four";
+        Assertions.assertEquals(
+                expected,
+                NumberToWords.getNumberToWords(1234)
+        );
+    }
+
+    @Test
+    public void testOneZeroOneZero() {
+        expected = "One Zero One Zero";
+        Assertions.assertEquals(
+                expected,
+                NumberToWords.getNumberToWords(1010)
+        );
+    }
+
+    @Test
+    public void TestOneZeroZeroZero() {
+        expected = "One Zero Zero Zero";
+        Assertions.assertEquals(
+                expected,
+                NumberToWords.getNumberToWords(1000)
+        );
+    }
+
+    @Test
+    public void testNegativeOneTwo() {
+        expected = "Invalid Value";
+        Assertions.assertEquals(
+                expected,
+                NumberToWords.getNumberToWords(-12)
+        );
+    }
+
+    @Test
+    public void testCourseDataForGetDigitCount() {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(
+                11, 0, 1, 7,
+                101, -222, 1010, 100,
+                123321, 10000, 37273, -37273,
+                1132273
+        ));
+        List<Integer> answers = new ArrayList<>(Arrays.asList(
+                2, 1, 1, 1,
+                3, -1, 4, 3,
+                6, 5, 5, -1,
+                7
+        ));
+
+        for (int index = 0; index < answers.size(); index++) {
+            int number = numbers.get(index);
+            int answer = answers.get(index);
+            Assertions.assertEquals(
+                    answer,
+                    NumberToWords.getDigitCount(number)
+            );
+        }
+    }
+
+    @Test
+    public void testCourseDataForReverse() {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(
+                0, 5, -2, -1,
+                24, 23, 107, 208,
+                -2521, 12345, 123, 323323,
+                157
+        ));
+        List<Integer> answers = new ArrayList<>(Arrays.asList(
+                0, 5, -2, -1,
+                42, 32, 701, 802,
+                -1252, 54321, 321, 323323,
+                751
+        ));
+
+        for (int index = 0; index < answers.size(); index++) {
+            int number = numbers.get(index);
+            int answer = answers.get(index);
+            Assertions.assertEquals(
+                    answer,
+                    NumberToWords.reverse(number)
             );
         }
     }
