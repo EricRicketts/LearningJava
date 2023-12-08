9737ac698411c821f4f597dc19b5db8511e4e549
Eric Ricketts <eric_ricketts@me.com>
2023-12-08 05:43:44 -0500

    Input using Scanner class, basic exception handling

--- JavaProgrammingMasterClass/Section6/ParsingValuesAndReadingInput/src/main/java/org/example/App.java
+++ JavaProgrammingMasterClass/Section6/ParsingValuesAndReadingInput/src/main/java/org/example/App.java
@@ -1,10 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
         int currentYear = 2023;
-        System.out.println(App.getInputFromConsole(currentYear));
+
+        try {
+            System.out.println(App.getInputFromConsole(currentYear));
+        } catch (NullPointerException e) {
+            System.out.println(getInputFromScanner(currentYear));
+        }
     }
 
     public static int stringToIntegerConversion() {
@@ -24,5 +31,42 @@ public class App {
         String age = String.valueOf(currentYear - Integer.parseInt(dateOfBirth));
 
         return "So you are " + age + " years old.";
+    }
+
+    public static String getInputFromScanner(int currentYear) {
+        int limitOfHumanAge = 125;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hi, what is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hi " + name + ", thanks for taking the course!!");
+        System.out.println("What year were you born? ");
+
+        boolean validDOB = false;
+        int age = 0;
+
+        do {
+            System.out.println("Enter a year of birth >= " +
+                            (currentYear - limitOfHumanAge) + " and <= " + (currentYear)
+                    );
+            try {
+                age = checkData(currentYear, scanner.nextLine());
+                validDOB = (age < 0) ? false : true;
+            } catch (NumberFormatException badUserData) {
+                System.out.println("Characters are not allowed!!!  Try again.");
+            }
+        } while (!validDOB);
+
+        return "So you are " + age + " years old.";
+    }
+
+    public static int checkData(int currentYear, String dateOfBirth) {
+        int limitOfHumanAge = 125;
+        int dob = Integer.parseInt(dateOfBirth);
+        int minimumYear = currentYear - limitOfHumanAge;
+
+        if ((dob < minimumYear) || (dob > currentYear)) return -1;
+
+        return (currentYear - dob);
     }
 }
