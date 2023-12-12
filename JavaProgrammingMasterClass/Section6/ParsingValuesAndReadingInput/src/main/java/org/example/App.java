d5354867cad08475d0a6d91a3141454fcda3c551
Eric Ricketts <eric_ricketts@me.com>
2023-12-12 18:52:02 -0500

    reading user input challenge

--- JavaProgrammingMasterClass/Section6/ParsingValuesAndReadingInput/src/main/java/org/example/App.java
+++ JavaProgrammingMasterClass/Section6/ParsingValuesAndReadingInput/src/main/java/org/example/App.java
@@ -6,12 +6,8 @@ public class App {
 
     public static void main(String[] args) {
         int currentYear = 2023;
-
-        try {
-            System.out.println(App.getInputFromConsole(currentYear));
-        } catch (NullPointerException e) {
-            System.out.println(getInputFromScanner(currentYear));
-        }
+//       System.out.println(getInputFromScanner(currentYear));
+        readAndSumFiveNumbersFromTheConsole();
     }
 
     public static int stringToIntegerConversion() {
@@ -34,13 +30,13 @@ public class App {
     }
 
     public static String getInputFromScanner(int currentYear) {
+        // java src/main/java/org/example/App.java
         int limitOfHumanAge = 125;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hi, what is your name? ");
         String name = scanner.nextLine();
         System.out.println("Hi " + name + ", thanks for taking the course!!");
-        System.out.println("What year were you born? ");
 
         boolean validDOB = false;
         int age = 0;
@@ -68,5 +64,30 @@ public class App {
         if ((dob < minimumYear) || (dob > currentYear)) return -1;
 
         return (currentYear - dob);
+    }
+
+    public static void readAndSumFiveNumbersFromTheConsole() {
+        int numberCount = 0;
+        int numberCountLimit = 5;
+        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(
+                "This program asks the user to input five integers and their sum will be printed."
+        );
+
+        do {
+            System.out.print("Enter Number #" + (numberCount + 1) + ": ");
+            String enteredNumericString =  scanner.nextLine();
+            Double enteredNumber = Double.parseDouble(enteredNumericString);
+            if (enteredNumber == Math.floor(enteredNumber)) {
+                sum += enteredNumber;
+                numberCount += 1;
+            } else {
+                System.out.println("Enter a whole number not a decimal number.");
+            }
+
+        } while (numberCount < numberCountLimit);
+
+        System.out.println("The sum of your five numbers is: " + sum);
     }
 }
