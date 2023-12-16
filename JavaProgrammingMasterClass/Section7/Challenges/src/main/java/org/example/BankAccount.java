265c1f59d5688fd9c11d7f5458dd5833f165d8d5
Eric Ricketts <eric_ricketts@me.com>
2023-12-15 21:31:54 -0500

    getters and setters for challenge

---
+++ JavaProgrammingMasterClass/Section7/Challenges/src/main/java/org/example/BankAccount.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import java.text.DecimalFormat;
+
+public class BankAccount {
+
+    private int number;
+    private double balance;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhone;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public double deposit(double deposit) {
+        this.setBalance(this.getBalance() + deposit);
+        return this.getBalance();
+    }
+
+    public double withdraw(double withdraw) {
+        double remainingBalance = this.getBalance() - withdraw;
+        if (remainingBalance < 0) return -1;
+        this.setBalance(remainingBalance);
+
+        return this.getBalance();
+    }
+}
---
+++ JavaProgrammingMasterClass/Section7/Challenges/src/test/java/org/example/BankAccountTest.java
@@ -0,0 +1,83 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BankAccountTest {
+
+    private final BankAccount bankAccount = new BankAccount();
+    @BeforeEach
+    public void setUp() {
+        bankAccount.setNumber(101491);
+        bankAccount.setCustomerName("Eric Ricketts");
+        bankAccount.setCustomerEmail("eric_ricketts@icloud.com");
+        bankAccount.setCustomerPhone("919-449-5529");
+        bankAccount.setBalance(1000);
+    }
+
+    @Test
+    public void testGetAndSetAccountNumber() {
+        Assertions.assertEquals(
+                101491,
+                bankAccount.getNumber()
+        );
+        bankAccount.setNumber(222345);
+        Assertions.assertEquals(
+                222345,
+                bankAccount.getNumber()
+        );
+    }
+
+    @Test
+    public void testGetAndSetCustomerName() {
+        Assertions.assertEquals(
+                "Eric Ricketts",
+                bankAccount.getCustomerName()
+        );
+        bankAccount.setCustomerName("Elmer Fudd");
+        Assertions.assertEquals(
+                "Elmer Fudd",
+                bankAccount.getCustomerName()
+        );
+    }
+
+    @Test
+    public void testGetAndSetCustomerEmail() {
+        Assertions.assertEquals(
+                "eric_ricketts@icloud.com",
+                bankAccount.getCustomerEmail()
+        );
+        bankAccount.setCustomerEmail("ewricketts@icloud.com");
+        Assertions.assertEquals(
+                "ewricketts@icloud.com",
+                bankAccount.getCustomerEmail()
+        );
+    }
+
+    @Test
+    public void testGetAndSetCustomerPhone() {
+        Assertions.assertEquals(
+                "919-449-5529",
+                bankAccount.getCustomerPhone()
+        );
+        bankAccount.setCustomerPhone("703-451-0235");
+        Assertions.assertEquals(
+                "703-451-0235",
+                bankAccount.getCustomerPhone()
+        );
+    }
+
+    @Test
+    public void testGetAndSetBalance() {
+        Assertions.assertEquals(
+                1000.0,
+                bankAccount.getBalance()
+        );
+        bankAccount.setBalance(1500.50);
+        Assertions.assertEquals(
+                1500.50,
+                bankAccount.getBalance()
+        );
+    }
+}
