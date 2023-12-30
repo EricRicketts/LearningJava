619fd0e7540dc282fbaf52bace81a5cccf44f41e
Eric Ricketts <eric_ricketts@me.com>
2023-12-30 08:15:19 -0500

    worker test get birth date

--- JavaProgrammingMasterClass/Section7/Challenges/Inheritance/src/test/java/org/example/WorkerTest.java
+++ JavaProgrammingMasterClass/Section7/Challenges/Inheritance/src/test/java/org/example/WorkerTest.java
@@ -4,9 +4,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 public class WorkerTest {
 
     private Worker worker;
+    private String expected, result, month, day, year;
+    private LocalDate birthDate, endDate;
+
+    private String addZeroToDayOfMonthLessThanTen(int dayOfMonth) {
+        return dayOfMonth < 10 ? "0" + Integer.toString(dayOfMonth) :
+            Integer.toString(dayOfMonth);
+    }
 
     @BeforeEach
     public void setUp() {
@@ -20,5 +29,16 @@ public class WorkerTest {
         Assertions.assertEquals(oldName, worker.getName());
         worker.setName(newName);
         Assertions.assertEquals(newName, worker.getName());
+    }
+
+    @Test
+    public void testGetBirthDate() {
+        expected = "11/06/1937";
+        birthDate = worker.getBirthDate();
+        day = addZeroToDayOfMonthLessThanTen(birthDate.getDayOfMonth());
+        month = Integer.toString(birthDate.getMonthValue());
+        year = Integer.toString(birthDate.getYear());
+        result = month + "/" + day + "/" + year;
+        Assertions.assertEquals(expected, result);
     }
 }
