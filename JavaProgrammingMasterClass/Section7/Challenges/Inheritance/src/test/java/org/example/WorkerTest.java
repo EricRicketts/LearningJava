7c03fb0accf6c4ae38d33459a3a9b13051699af9
Eric Ricketts <eric_ricketts@me.com>
2023-12-31 08:03:38 -0500

    get and set birth date and end date

--- JavaProgrammingMasterClass/Section7/Challenges/Inheritance/src/test/java/org/example/WorkerTest.java
+++ JavaProgrammingMasterClass/Section7/Challenges/Inheritance/src/test/java/org/example/WorkerTest.java
@@ -5,12 +5,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class WorkerTest {
 
+    final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy").withLocale(Locale.US);
     private Worker worker;
     private String expected, result, month, day, year;
     private LocalDate birthDate, endDate;
+
+    private String extractDateStringFromLocalDate(LocalDate date) {
+        String day = addZeroToDayOfMonthLessThanTen(date.getDayOfMonth());
+        String month = Integer.toString(date.getMonthValue());
+        String year = Integer.toString(date.getYear());
+        return month + "/" + day + "/" + year;
+    }
 
     private String addZeroToDayOfMonthLessThanTen(int dayOfMonth) {
         return dayOfMonth < 10 ? "0" + Integer.toString(dayOfMonth) :
@@ -35,10 +45,30 @@ public class WorkerTest {
     public void testGetBirthDate() {
         expected = "11/06/1937";
         birthDate = worker.getBirthDate();
-        day = addZeroToDayOfMonthLessThanTen(birthDate.getDayOfMonth());
-        month = Integer.toString(birthDate.getMonthValue());
-        year = Integer.toString(birthDate.getYear());
-        result = month + "/" + day + "/" + year;
+        result = extractDateStringFromLocalDate(birthDate);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testSetBirthDate() {
+        expected = "10/18/2020";
+        worker.setBirthDate(LocalDate.parse(expected, formatter));
+        result = extractDateStringFromLocalDate(worker.getBirthDate());
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testGetEndDate() {
+        expected = "12/31/2030";
+        result = extractDateStringFromLocalDate(worker.getEndDate());
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testSetEndDate() {
+        expected = "12/01/2025";
+        worker.setEndDate(LocalDate.parse(expected, formatter));
+        result = extractDateStringFromLocalDate(worker.getEndDate());
         Assertions.assertEquals(expected, result);
     }
 }
