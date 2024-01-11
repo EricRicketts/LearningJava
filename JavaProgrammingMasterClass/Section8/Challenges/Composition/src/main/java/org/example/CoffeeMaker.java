52dac54b4949fd0da231e55c6d3ff527b03aebe7
Eric Ricketts <eric_ricketts@me.com>
2024-01-11 08:10:09 -0500

    brewCoffee method on CoffeeMaker class

--- JavaProgrammingMasterClass/Section8/Challenges/Composition/src/main/java/org/example/CoffeeMaker.java
+++ JavaProgrammingMasterClass/Section8/Challenges/Composition/src/main/java/org/example/CoffeeMaker.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class CoffeeMaker extends ApplianceState {
+
+    public String brewCoffee() {
+        return this.isHasWorkToDo() ? "The Coffee maker is brewing coffee." : "";
+    }
 }
--- JavaProgrammingMasterClass/Section8/Challenges/Composition/src/test/java/org/example/CoffeeMakerTest.java
+++ JavaProgrammingMasterClass/Section8/Challenges/Composition/src/test/java/org/example/CoffeeMakerTest.java
@@ -1,7 +1,27 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CoffeeMakerTest {
+
+    CoffeeMaker coffeeMaker;
+
+    @BeforeEach
+    public void setUp() {
+        coffeeMaker = new CoffeeMaker();
+    }
+
+    @Test
+    public void testNoWorkForCoffeeMaker() {
+        String expected, result;
+        expected = "";
+        result = coffeeMaker.brewCoffee();
+        Assertions.assertEquals(expected, result);
+        expected = "The Coffee maker is brewing coffee.";
+        coffeeMaker.setHasWorkToDo(true);
+        result = coffeeMaker.brewCoffee();
+        Assertions.assertEquals(expected, result);
+    }
 }
