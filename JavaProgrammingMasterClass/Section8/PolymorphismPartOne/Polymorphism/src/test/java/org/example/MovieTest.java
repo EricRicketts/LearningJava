628691860ce0b87152d7a5f12749e2901ba5d978
Eric Ricketts <eric_ricketts@me.com>
2024-01-13 10:10:47 -0500

    Movie Class and tests

---
+++ JavaProgrammingMasterClass/Section8/PolymorphismPartOne/Polymorphism/src/test/java/org/example/MovieTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MovieTest {
+
+    private Movie movie;
+
+    @BeforeEach
+    public void setUp() {
+        movie = new Movie("Pee Wee's Big Adventure");
+    }
+
+    @Test
+    public void testWatchMovie() {
+        String expected = "Pee Wee's Big Adventure is a Movie film.";
+        String result = movie.watchMovie();
+        Assertions.assertEquals(expected, result);
+    }
+}
