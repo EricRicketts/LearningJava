e1dc53944c4f39c1b0d52aeee6fb890376201695
Eric Ricketts <eric_ricketts@me.com>
2024-01-13 05:43:21 -0500

    Section 8 Challenger Printer Class and its tests

--- JavaProgrammingMasterClass/Section8/Challenges/Encapsulation/src/test/java/org/example/PrinterTest.java
+++ JavaProgrammingMasterClass/Section8/Challenges/Encapsulation/src/test/java/org/example/PrinterTest.java
@@ -4,13 +4,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
+
 public class PrinterTest {
 
-    private Printer printer;
+    private Printer printer, printerDuplex;
 
     @BeforeEach
     public void setUp() {
         printer = new Printer(50, 100, false);
+        printerDuplex = new Printer(50, 100, true);
     }
 
     @Test
@@ -35,5 +38,25 @@ public class PrinterTest {
     public void testNonDuplexPrinter() {
         Assertions.assertEquals(17, printer.printPages(17));
         Assertions.assertEquals(117, printer.getPagesPrinted());
+    }
+
+    @Test
+    public void testDuplexPrinterEvenPages() throws Exception {
+        String text = tapSystemOut(() -> {
+            printerDuplex.printPages(20);
+        });
+        Assertions.assertEquals(110, printerDuplex.getPagesPrinted());
+        Assertions.assertEquals("Duplex printer.", text.trim());
+    }
+
+    @Test
+    public void testDuplexPrinterOddPages() throws Exception {
+        // if given 31 pages to print the duplex printer should print
+        // 15 * 2 = 30 pages plus 1 remaining page for a total of 16 pages
+        String text = tapSystemOut(() -> {
+            printerDuplex.printPages(31);
+        });
+        Assertions.assertEquals(116, printerDuplex.getPagesPrinted());
+        Assertions.assertEquals("Duplex printer.", text.trim());
     }
 }
