c000dd5b04c256a3d71dfe05128a9fb4d7b17de4
Eric Ricketts <eric_ricketts@me.com>
2024-02-14 04:58:48 -0500

    test items of meal

--- JavaProgrammingMasterClass/Section8/Challenges/MasterChallenge/src/test/java/org/example/MealTest.java
+++ JavaProgrammingMasterClass/Section8/Challenges/MasterChallenge/src/test/java/org/example/MealTest.java
@@ -6,12 +6,21 @@ import org.junit.jupiter.api.Test;
 
 public class MealTest {
 
-    private Meal defaultMeal;
+    private Meal defaultMeal, meal;
     Object[] expected, results;
+    double expectedPrice, resultantPrice;
+    String expectedList, resultantList;
 
     @BeforeEach
     public void setUp() {
         defaultMeal = new Meal();
+        meal = new Meal(
+                new Hamburger("Large"),
+                new Item("Medium", "Drink", "Coke"),
+                new Item("Medium", "Side", "Fries"));
+        meal.setFirstTopping(new Item("", "Topping", "Cheese"));
+        meal.setSecondTopping(new Item("", "Topping", "Ketchup"));
+        meal.setThirdTopping(new Item("", "Topping", "Mustard"));
     }
 
     @Test
@@ -31,20 +40,52 @@ public class MealTest {
 
     @Test
     public void testDefaultMealPrice() {
-        double expectedPrice = 9.99 + 2.99 + 2.99;
-        double resultantPrice = defaultMeal.getPrice();
+        expectedPrice = 9.99 + 2.99 + 2.99;
+        resultantPrice = defaultMeal.getPrice();
         Assertions.assertEquals(expectedPrice, resultantPrice);
     }
 
     @Test
     public void testDefaultMealPrintItemizedList() {
-        String expectedList = """
+        expectedList = """
                 Medium Hamburger $9.99
                 Small Coke $2.99
                 Small Fries $2.99
                 Total $15.97
                 """;
-        String resultantList = defaultMeal.printItemizedList();
+        resultantList = defaultMeal.printItemizedList();
         Assertions.assertEquals(expectedList, resultantList);
+    }
+
+    @Test
+    public void testMealItems() {
+        expected = new Object[]{
+            new Hamburger("Large"),
+            new Item("Medium", "Drink", "Coke"),
+            new Item("Medium", "Side", "Fries"),
+            new Item("", "Topping", "Cheese"),
+            new Item("", "Topping", "Ketchup"),
+            new Item("", "Topping", "Mustard")
+        };
+        results = new Object[]{
+            meal.getHamburger(),
+            meal.getDrink(),
+            meal.getSide(),
+            meal.getFirstTopping(),
+            meal.getSecondTopping(),
+            meal.getThirdTopping()
+        };
+        Assertions.assertArrayEquals(expected, results);
+    }
+
+    @Test
+    public void testTotalPrice() {
+        expectedPrice = 10.99 + (1.50 + 1.00 + 1.00) + 4.99 + 4.99;
+        resultantPrice = meal.getPrice();
+        Assertions.assertEquals(expectedPrice, resultantPrice);
+    }
+
+    @Test
+    public void testPrintItemizedList() {
     }
 }
