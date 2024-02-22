91df2ef3a8e7275ba080c1cfc4e94cb454a62c64
Eric Ricketts <eric_ricketts@me.com>
2024-02-22 15:58:32 -0500

    relationship between array and list created from array

--- JavaProgrammingMasterClass/Section10/ListsIteratorsAutoboxing/src/test/java/org/example/MoreListsTest.java
+++ JavaProgrammingMasterClass/Section10/ListsIteratorsAutoboxing/src/test/java/org/example/MoreListsTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -27,9 +29,7 @@ public class MoreListsTest {
         String expected = "java.util.ImmutableCollections$ListN";
         String result = stringList.getClass().getName();
         Assertions.assertEquals(expected, result);
-        Exception exception = assertThrows(UnsupportedOperationException.class, () -> {
-            stringList.add("yogurt");
-        });
+        Exception exception = assertThrows(UnsupportedOperationException.class, () -> stringList.add("yogurt"));
         String expectedExceptionString = "java.lang.UnsupportedOperationException";
         String resultantExceptionString = exception.toString();
         Assertions.assertEquals(expectedExceptionString, resultantExceptionString);
@@ -79,6 +79,7 @@ public class MoreListsTest {
         // apples, bananas, milk, eggs
         ArrayList<String> newList = new ArrayList<>(stringList);
         boolean listWasAdded = newList.addAll(List.of("yogurt", "cabbage", "brocolli", "carrots"));
+        Assertions.assertTrue(listWasAdded);
         Assertions.assertEquals("yogurt", newList.get(4));
         Assertions.assertEquals(5, newList.indexOf("cabbage"));
         Assertions.assertEquals(6, newList.lastIndexOf("brocolli"));
@@ -89,6 +90,7 @@ public class MoreListsTest {
         // apples, bananas, milk, eggs
         ArrayList<String> newList = new ArrayList<>(stringList);
         boolean listWasAdded = newList.addAll(List.of("yogurt", "cabbage", "brocolli", "carrots"));
+        Assertions.assertTrue(listWasAdded);
         Assertions.assertEquals(8, newList.size());
         newList.remove(0); // remove apples
         Assertions.assertEquals(-1, newList.indexOf("apples"));
@@ -103,7 +105,64 @@ public class MoreListsTest {
         Assertions.assertFalse(listChanges);
         Assertions.assertEquals(3, newList.size());
         newList.clear();
-        Assertions.assertTrue(newList.isEmpty());
+        Assertions.assertTrue(newList.toString().contains("[]"));
+    }
+
+    @Test
+    public void testConvertListToArray() {
+        ArrayList<String> newList = new ArrayList<>(stringList);
+        String[] newArray = newList.toArray(new String[0]);
+
+        Assertions.assertArrayEquals(stringArray, newArray);
+    }
+
+    @Test
+    public void testSortAnArray() {
+        String[] expected = new String[]{"apples", "bananas", "eggs", "milk"};
+        Assertions.assertFalse(Arrays.equals(stringArray, expected));
+        Arrays.sort(stringArray);
+        Assertions.assertArrayEquals(expected, stringArray);
+    }
+
+    @Test
+    public void testSortList() {
+        // apples, bananas, milk, eggs
+        ArrayList<String> newList = new ArrayList<>(stringList);
+        boolean listWasAdded = newList.addAll(List.of("yogurt", "cabbage", "brocolli", "carrots"));
+        // apples, bananas, milk, eggs, yogurt, cabbage, brocolli, carrots
+        ArrayList<String> sortedList = new ArrayList<>(List.of(
+                "apples", "bananas", "brocolli", "cabbage", "carrots", "eggs", "milk", "yogurt"
+        ));
+        ArrayList<String> reverseSortedList = new ArrayList<>(List.of(
+                "yogurt", "milk", "eggs", "carrots", "cabbage", "brocolli", "bananas", "apples"
+        ));
+        Assertions.assertTrue(listWasAdded);
+        newList.sort(Comparator.naturalOrder());
+        Assertions.assertEquals(sortedList, newList);
+        newList.sort(Comparator.reverseOrder());
+        Assertions.assertEquals(reverseSortedList, newList);
+    }
+
+    @Test
+    public void testArrayAndImmutableListRelationship() {
+        String[] array = new String[]{"Zero", "One", "Two", "Three"};
+        List<String> list = Arrays.asList(array);
+        // as expected not resizable
+        Exception exception =
+                assertThrows(UnsupportedOperationException.class, () -> list.add("Four"));
+        Assertions.assertTrue(exception.toString().contains("UnsupportedOperationException"));
+        // changing list changes array, the list and array are linked to one another
+        // change the list changes the array
+        list.set(0, "0");
+        String[] expected = new String[]{"0", "0"};
+        String[] result = new String[]{list.get(0), array[0]};
+        Assertions.assertArrayEquals(expected, result);
+
+        // change the array changes the list
+        array[1] = "1";
+        expected = new String[]{"1", "1"};
+        result = new String[]{list.get(1), array[1]};
+        Assertions.assertArrayEquals(expected, result);
     }
 
 }
