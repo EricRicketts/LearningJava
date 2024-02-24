51b9fd4258a15397ef12c38e9e949cf0b446ed32
Eric Ricketts <eric_ricketts@me.com>
2024-02-24 14:27:30 -0500

    addNewContact for a new contact and a duplicate contact

--- JavaProgrammingMasterClass/Section10/ListsIteratorsAutoboxing/src/main/java/org/example/MobilePhone.java
+++ JavaProgrammingMasterClass/Section10/ListsIteratorsAutoboxing/src/main/java/org/example/MobilePhone.java
@@ -1,4 +1,42 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class MobilePhone {
+
+    private final String myNumber;
+    private final ArrayList<Contact> myContacts;
+
+    private boolean sameContact(Contact firstContact, Contact secondContact) {
+        return firstContact.getName().equals(secondContact.getName());
+    }
+
+    private int findContact(Contact contact) {
+        return this.myContacts.indexOf(contact.getName());
+    }
+
+    private int findContact(String name) {
+        return this.myContacts.indexOf(name);
+    }
+
+    public boolean addNewContact(Contact newContact) {
+        boolean contactExists = false;
+        for (Contact currentContact : myContacts) {
+            if (this.sameContact(currentContact, newContact)) {
+                contactExists = true;
+                break;
+            }
+        }
+        if (!contactExists) {
+            myContacts.add(newContact);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<>();
+    }
 }
--- JavaProgrammingMasterClass/Section10/ListsIteratorsAutoboxing/src/test/java/org/example/MobilePhoneTest.java
+++ JavaProgrammingMasterClass/Section10/ListsIteratorsAutoboxing/src/test/java/org/example/MobilePhoneTest.java
@@ -1,4 +1,34 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 public class MobilePhoneTest {
+
+    private MobilePhone mobilePhone;
+    private Contact contact;
+
+    @BeforeEach
+    public void setUp() {
+        mobilePhone = new MobilePhone("919-449-5529");
+        mobilePhone.addNewContact(new Contact("Elmer Fudd", "703-451-0235"));
+        mobilePhone.addNewContact(new Contact("Bugs Bunny", "703-553-1295"));
+        mobilePhone.addNewContact(new Contact("Daffy Duck", "703-761-9385"));
+        mobilePhone.addNewContact(new Contact("Foghorn Leghorn", "703-553-7844"));
+        mobilePhone.addNewContact(new Contact("Yosemite Sam", "703-451-2299"));
+    }
+
+    @Test
+    public void testAddNewContact() {
+        contact = new Contact("Speedy Gonzales", "703-451-4976");
+        Assertions.assertTrue(mobilePhone.addNewContact(contact));
+    }
+
+    @Test
+    public void testCannotAddDuplicateContact() {
+        contact = new Contact("Daffy Duck", "999-335-1234");
+        Assertions.assertFalse(mobilePhone.addNewContact(contact));
+    }
 }
