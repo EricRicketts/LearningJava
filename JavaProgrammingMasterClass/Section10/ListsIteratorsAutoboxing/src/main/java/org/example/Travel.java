ad9957f35b28c91554da5241839babc9beb77d19
Eric Ricketts <eric_ricketts@me.com>
2024-02-28 04:49:10 -0500

    create linked list for challenge

---
+++ JavaProgrammingMasterClass/Section10/ListsIteratorsAutoboxing/src/main/java/org/example/Travel.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.*;
+
+public class Travel {
+
+    private final List<Map<String, Integer>> distancesFromSydney;
+
+    public List<Map<String, Integer>> getDistancesFromSydney() {
+        return distancesFromSydney;
+    }
+
+    public Travel() {
+        distancesFromSydney = new LinkedList<>();
+        Object[][] data = new Object[][]{
+                {"Sydney", 0}, {"Melbourne", 877},
+                {"Brisbane", 917}, {"Adelaide", 1374},
+                {"Alice Springs", 2771}, {"Perth", 3923},
+                {"Darwin", 3972}
+        };
+        for (Object[] element : data) {
+            Map<String, Integer> map = new HashMap<>();
+            String key = (String) element[0];
+            Integer value = (Integer) element[1];
+            map.put(key, value);
+            distancesFromSydney.add(map);
+        }
+    }
+}
---
+++ JavaProgrammingMasterClass/Section10/ListsIteratorsAutoboxing/src/test/java/org/example/LinkedListChallengeTest.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+
+public class LinkedListChallengeTest {
+
+    private Travel travel;
+
+    @BeforeEach
+    public void setUp() {
+        travel = new Travel();
+    }
+
+    @Test
+    public void testListOfMaps() {
+        List<Map<String, Integer>> listOfMaps = travel.getDistancesFromSydney();
+        ListIterator<Map<String, Integer>> iterator = listOfMaps.listIterator();
+        Object[][] expectedData = new Object[][]{
+                {"Sydney", 0}, {"Melbourne", 877},
+                {"Brisbane", 917}, {"Adelaide", 1374},
+                {"Alice Springs", 2771}, {"Perth", 3923},
+                {"Darwin", 3972}
+        };
+        int index = 0;
+        while (iterator.hasNext()) {
+            Object[] expected = expectedData[index];
+            String expectedKey = (String) expected[0];
+            Integer expectedValue = (Integer) expected[1];
+            Map<String, Integer> map = iterator.next();
+            Assertions.assertTrue(map.containsKey(expectedKey));
+            Assertions.assertEquals(map.get(expectedKey), expectedValue);
+            index += 1;
+        }
+    }
+}
