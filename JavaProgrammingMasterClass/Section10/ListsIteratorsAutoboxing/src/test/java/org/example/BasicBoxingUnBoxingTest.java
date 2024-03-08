99efa2491825848e850f798e41cd90d8f2d57aeb
Eric Ricketts <eric_ricketts@me.com>
2024-03-08 04:42:22 -0500

    examples of boxing and unboxing lists

--- JavaProgrammingMasterClass/Section10/ListsIteratorsAutoboxing/src/test/java/org/example/BasicBoxingUnBoxingTest.java
+++ JavaProgrammingMasterClass/Section10/ListsIteratorsAutoboxing/src/test/java/org/example/BasicBoxingUnBoxingTest.java
@@ -3,7 +3,19 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BasicBoxingUnBoxingTest {
+
+    public static ArrayList<Integer> getList(int... varargs) {
+        ArrayList<Integer> aList = new ArrayList<>();
+        for (int i : varargs) {
+            aList.add(i);
+        }
+
+        return aList;
+    }
 
     @Test
     public void testFactoryMethod() {
@@ -18,5 +30,41 @@ public class BasicBoxingUnBoxingTest {
         int autoUnboxedInt = autoBoxedInt;
        Assertions.assertEquals("java.lang.Integer", Integer.valueOf(autoUnboxedInt).getClass().getName());
        // best we can do here is to manually box the primitive value and then see if we get the boxed class
+    }
+
+    @Test
+    public void testAutoBoxingForArrays() {
+        Integer[] integerWrapperArray = new Integer[5];
+        // the assignments to each individual array location are autoboxed, no need to manually box the values
+        integerWrapperArray[0] = 10;
+        integerWrapperArray[1] = 20;
+        integerWrapperArray[2] = 30;
+        integerWrapperArray[3] = 40;
+        integerWrapperArray[4] = 50;
+        Assertions.assertEquals("java.lang.Integer", integerWrapperArray[0].getClass().getName());
+    }
+
+    @Test
+    public void testAutoboxingForArrayLiteralAssignments() {
+        // the literal assignment is of primitive type character, but Java autoboxed each character primitive to
+        // a Character class, because the array was of type Character
+        Character[] characterWrapperArray = {'a', 'b', 'c', 'd', 'e'};
+        Assertions.assertEquals("java.lang.Character", characterWrapperArray[4].getClass().getName());
+    }
+
+    @Test
+    public void testManuallyBoxIntValues() {
+        // in the method we manually boxed the primitive arguments, since the method was declared to return
+        // a list of Integer class then all elements in the list will be of the Integer class.
+        Assertions.assertEquals("java.lang.Integer",
+                getList(1, 2, 3, 4, 5).get(3).getClass().getName());
+    }
+
+    @Test
+    public void testAutomaticBoxingForAList() {
+        // we can do automatically what the method under test did in the above assertion
+        // Java will automatically box a list of primitives if the receiving list is of the wrapper class type
+        List<Integer> aList = List.of(1, 2, 3, 4, 5);
+        Assertions.assertEquals("java.lang.Integer", aList.get(4).getClass().getName());
     }
 }
