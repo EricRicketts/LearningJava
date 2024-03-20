f9ed96f2141873fc52bccd6d45d9fc7fae6e613f
Eric Ricketts <eric_ricketts@me.com>
2024-03-20 17:38:34 -0400

    test Trackable and FlightEnabled objects

--- JavaProgrammingMasterClass/Section11/AbstractionsInJava/src/main/java/org/example/Bird.java
+++ JavaProgrammingMasterClass/Section11/AbstractionsInJava/src/main/java/org/example/Bird.java
@@ -6,4 +6,24 @@ public class Bird extends GenericAnimal implements FlightEnabled, Trackable {
     public String move() {
         return getClass().getSimpleName() + " moved.";
     }
+
+    @Override
+    public String takeOff() {
+        return getClass().getSimpleName() + " took off from the nest.";
+    }
+
+    @Override
+    public String land() {
+        return getClass().getSimpleName() + " landed in the nest.";
+    }
+
+    @Override
+    public String fly() {
+        return getClass().getSimpleName() + " is flying over the farm.";
+    }
+
+    @Override
+    public String track() {
+        return getClass().getSimpleName() + " has a tracking device attached.";
+    }
 }
--- JavaProgrammingMasterClass/Section11/AbstractionsInJava/src/test/java/org/example/InterfacesTest.java
+++ JavaProgrammingMasterClass/Section11/AbstractionsInJava/src/test/java/org/example/InterfacesTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class InterfacesTest {
 
     private Bird bird;
@@ -37,6 +39,9 @@ public class InterfacesTest {
         A class can only extend a single class (so Java is called Single Inheritance) but it can implement many
         interfaces (this is called plug and play) and is what makes Interfaces so powerful.
 
+        In this case the flier object implements the methods from FlightEnabled, they are called from bird
+        as bird is the runtime object.  Likewise, the bird object is type Trackable implements the methods defined
+        in the Trackable interface, they are called from the bird as bird is the runtime object.
     */
 
     @Test
@@ -48,4 +53,24 @@ public class InterfacesTest {
     public void testGenericAnimalObject() {
         Assertions.assertEquals("Bird moved.", animal.move());
     }
+
+    @Test
+    public void testFlightEnabledObject() {
+        String[] expected = new String[]{
+                "Bird took off from the nest.", "Bird landed in the nest.", "Bird is flying over the farm."
+        };
+        String[] results = new String[]{
+                flier.takeOff(), flier.land(), flier.fly()
+        };
+        Assertions.assertArrayEquals(expected, results);
+    }
+
+    @Test
+    public void testTrackableObject() {
+        Assertions.assertEquals("Bird has a tracking device attached.", tracked.track());
+    }
+    /*
+        I cannot test calling a Trackable method from a FlightEnabled object because a compiler error is
+        immediately thrown.  flier.track() shows a compile error.
+    */
 }
