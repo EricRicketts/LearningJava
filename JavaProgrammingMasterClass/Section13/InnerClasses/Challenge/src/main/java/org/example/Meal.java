7c5334e6913d1fda0f3e5749ae3d2be6d802e399
Eric Ricketts <eric_ricketts@me.com>
2024-12-14 15:49:55 -0500

    finish comments on Inner Classes

--- JavaProgrammingMasterClass/Section13/InnerClasses/Challenge/src/main/java/org/example/Meal.java
+++ JavaProgrammingMasterClass/Section13/InnerClasses/Challenge/src/main/java/org/example/Meal.java
@@ -5,6 +5,15 @@ import java.util.List;
 
 public class Meal {
 
+    /*
+        Meal is the only public facing class.  Item and its subclass, Burger, are both private.
+        The design decision is to make all the calculations regarding the individual meal items
+        within the Meal class.
+        The Meal class then takes the results of the Inner class calculations to produce the public
+        facing results.  This is a very good example of encapsulation, but one drawback is a lack of
+        flexibility.  If there is an Item not on the order then it needs to be added as a class to
+        the Inner classes of Meal.
+    */
     private double price = 5.0;
     private Burger burger;
     private Item drink;
@@ -12,20 +21,20 @@ public class Meal {
 
     private double conversionRate;
 
+    // no argument constructor calls parameter based constructor with conversion rate of 1
     public Meal() {
         this(1);
     }
 
+    // base constructor only argument is conversionRate, other items are fixed
     public Meal(double conversionRate) {
         this.conversionRate = conversionRate;
         burger = new Burger("regular");
         drink = new Item("coke", "drink", 1.5);
-        System.out.println(drink.name);
         side = new Item("fries", "side", 2.0);
     }
 
     public double getTotal() {
-
         double total = burger.getPrice() + drink.price + side.price;
         return Item.getPrice(total, conversionRate);
     }
@@ -46,6 +55,8 @@ public class Meal {
         private String type;
         private double price;
 
+        // two argument constructor if the item is a burger, it does have an issue if the
+        // type is not a burger, set the meal price to zero
         public Item(String name, String type) {
             this(name, type, type.equals("burger") ? Meal.this.price : 0);
         }
@@ -69,6 +80,9 @@ public class Meal {
 
     private class Burger extends Item {
 
+        private static String noToppingError = "";
+
+        // adding code after enum, get the price of the chosen topping
         private enum Extra {AVOCADO, BACON, CHEESE, KETCHUP, MAYO, MUSTARD, PICKLES;
 
             private double getPrice() {
@@ -86,8 +100,9 @@ public class Meal {
             super(name, "burger", 5.0);
         }
 
+        // get the price of the main items by calling the parent class => Item
+        // then add the price of all the toppings
         public double getPrice() {
-
             double total = super.price;
             for (Item topping : toppings) {
                 total += topping.price;
@@ -97,20 +112,22 @@ public class Meal {
 
         private void addToppings(String... selectedToppings) {
 
+            // iterate through each individual toppings and add an individual topping
+            // to the toppings list, this is so that the getPrice() can calculate the
+            // price of the included toppings
             for (String selectedTopping : selectedToppings) {
                 try {
                     Extra topping = Extra.valueOf(selectedTopping.toUpperCase());
                     toppings.add(new Item(topping.name(), "TOPPING",
                             topping.getPrice()));
                 } catch (IllegalArgumentException ie) {
-                    System.out.println("No topping found for " + selectedTopping);
+                    noToppingError = "No topping found for " + selectedTopping;
                 }
             }
         }
 
         @Override
         public String toString() {
-
             StringBuilder itemized = new StringBuilder(super.toString());
             for (Item topping : toppings) {
                 itemized.append("\n");
